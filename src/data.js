3f4f53a663b64b684978cd487b0e5073133d78a0
Brandon Lancaster <>
2019-06-03 15:46:00 -0500

    moved fs to data file

--- src/data.js
+++ src/data.js
@@ -3,3 +3,13 @@ const path = require('path');
 
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
 const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const users = JSON.parse(userData);
+
+const writeJSON = () => {
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+}
+
+module.exports = { accounts, users, writeJSON };
